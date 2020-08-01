223f7ff583654488ce3eb436ae9bd0aadfa8dc9a
Graham Mothersill <graham.mothersill@gmail.com>
2020-08-01 16:01:42 -0700

    testing commit and push

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload. <b>BAM BAM</b> WE'RE HERE WE ARE AWESOME.
         </p>
         <a
           className="App-link"
