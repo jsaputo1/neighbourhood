e8fbc14457e822ce692a0276e668fb5ff78ea794
John Saputo <jsaputo1@gmail.com>
2020-08-03 23:21:37 +0000

    updated map query to include events, alerts, services and users

--- server/src/routes/map.js
+++ server/src/routes/map.js
@@ -4,8 +4,9 @@ module.exports = db => {
   router.get("/", (request, response) => {
     db.query(
       `
-      SELECT *
-      FROM events;
+      SELECT events.*, alerts.*, services.*, users.*
+      FROM events
+      JOIN 
     `
     ).then(({ rows: allListings }) => {
       response.json(allListings);
@@ -14,3 +15,11 @@ module.exports = db => {
 
   return router;
 };
+
+
+
+// SELECT listings.*, favorites.*
+//   FROM favorites;
+// JOIN listings ON favorites.listing_id = listings.id;
+// JOIN buyers ON favorites.buyer_id = buyers.id;
+// WHERE buyers.email = $1;
