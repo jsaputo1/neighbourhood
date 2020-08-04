819e664a491a660ea3f518d831f9a3b63a00dd8c
jsaputo1 <59458399+jsaputo1@users.noreply.github.com>
2020-08-04 19:49:37 -0400

    create shell for select neighbourhood page

--- client/src/components/Landing/SelectNeighbourhood.js
+++ client/src/components/Landing/SelectNeighbourhood.js
@@ -1,7 +1,21 @@
 import React from "react";
+import "../../styles.scss";
 
 function SelectNeighbourhood() {
-  return <h1>Select Neighbourhood</h1>;
+  return <div className="select-neighbourhood-container">
+    <h3>Based on your location, we suggest joining one of the following neighbourhoods </h3>
+    <div className="neighbourhood-choices">
+      <figure>
+        <img src=""></img>
+        <h4>Choice 1</h4>
+      </figure>
+      <figure>
+        <img src=""></img>
+        <h4>Choice 2</h4>
+      </figure>
+    </div>
+  </div>;
+
 }
 
 export default SelectNeighbourhood;
