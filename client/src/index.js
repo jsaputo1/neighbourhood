7283020eddd0802dc5a571ec5a4eadb41e95c7c0
Samy0412 <samantha.gadet@gmail.com>
2020-08-04 12:21:55 -0400

    littel correction

--- client/src/index.js
+++ client/src/index.js
@@ -5,7 +5,7 @@ import App from "./components/App";
 //That is needed to style the material-ui components (Button for exemple)
 import { ThemeProvider } from "@material-ui/core/styles";
 import theme from "./theme";
-//That is needed if we use react bootstrap
+//That is needed if we use react-bootstrap
 import "bootstrap/dist/css/bootstrap.min.css";
 //That is our own styling sheet
 import "./styles.scss";
