024f007464195a6cd7c7371c206b83a022a528ab
jsaputo1 <59458399+jsaputo1@users.noreply.github.com>
2020-08-04 22:41:34 -0400

    add code to show first 2 neighbourhood options

--- client/src/components/Landing/SelectNeighbourhood.js
+++ client/src/components/Landing/SelectNeighbourhood.js
@@ -1,21 +1,39 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import "../../styles.scss";
 
 function SelectNeighbourhood() {
-  return <div className="select-neighbourhood-container">
+
+  const [neighbourhoods, setNeighbourhoods] = useState([]);
+
+  useEffect(() => {
+    axios.get("/neighbourhood/choices")
+      .then(
+        (response) => {
+          // console.log("Data:", response.data);
+          setNeighbourhoods(response.data);
+        }
+      );
+  }, []);
+
+  console.log("neighbourhoods:", neighbourhoods);
+  // console.log("neighbourhoods 0 index:", Object.keys(neighbourhoods[0]));
+
+  return (!neighbourhoods[0] ? null : (<div className="select-neighbourhood-container">
     <h3>Based on your location, we suggest joining one of the following neighbourhoods </h3>
     <div className="neighbourhood-choices">
       <figure>
-        <img src=""></img>
-        <h4>Choice 1</h4>
+        <img src={neighbourhoods[0].neighbourhood_photo}></img>
+        <h4>{neighbourhoods[0].name}</h4>
       </figure>
       <figure>
-        <img src=""></img>
-        <h4>Choice 2</h4>
+        <img src={neighbourhoods[1].neighbourhood_photo}></img>
+        <h4>{neighbourhoods[1].name}</h4>
       </figure>
     </div>
-  </div>;
-
+  </div>
+  )
+  );
 }
 
 export default SelectNeighbourhood;
