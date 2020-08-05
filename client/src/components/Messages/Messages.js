9dcdbf0acb51df1dba7cc6afd26fab86e4d64965
jsaputo1 <59458399+jsaputo1@users.noreply.github.com>
2020-08-05 14:05:17 -0400

    import use state, axios, redirect and stylesheet

--- client/src/components/Messages/Messages.js
+++ client/src/components/Messages/Messages.js
@@ -1,7 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
+import { Redirect } from "react-router-dom";
+import axios from "axios";
 
-function Messages() {
-  return <h1>Messages</h1>;
+//Our own style sheet
+import "../../styles.scss";
+
+function Messages(props) {
+  return <div className="messages-container">
+    <h3>Hello {props.user.first_name}</h3>
+  </div>;
+
 }
 
 export default Messages;
