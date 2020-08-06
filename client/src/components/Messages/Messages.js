f99d8a89a55704bdd437c3ce796a8284fca34baf
jsaputo1 <59458399+jsaputo1@users.noreply.github.com>
2020-08-05 22:45:51 -0400

    add messages state. display messages

--- client/src/components/Messages/Messages.js
+++ client/src/components/Messages/Messages.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Redirect } from "react-router-dom";
 import axios from "axios";
 
@@ -6,8 +6,32 @@ import axios from "axios";
 import "../../styles.scss";
 
 function Messages(props) {
+  const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    axios.get("/messages/userMessages")
+      .then(
+        (response) => {
+          setMessages(response.data);
+          console.log(response.data);
+        }
+      );
+  }, []);
+
   return <div className="messages-container">
     <h3>Hello {props.user.first_name}</h3>
+    <div className="individual-message">
+      {messages.map(i => (
+        <figure key={i.id}>
+          <h6>Conversation ID: {i.conversation_id}</h6>
+          <h6>Sent By: {i.sender_id}</h6>
+          <h6>Received By: {i.receiver_id}</h6>
+          <h6>Message: {i.message_text}</h6>
+          <h6>Time Sent: {i.time_sent}</h6>
+        </figure>
+      ))}
+    </div>
+
   </div>;
 
 }
