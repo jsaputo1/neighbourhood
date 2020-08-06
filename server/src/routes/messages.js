f8abd8931020cf2d567cb707b0852e1e20d09a53
jsaputo1 <59458399+jsaputo1@users.noreply.github.com>
2020-08-06 13:01:23 -0400

    create groupBy function and import to messages route file

--- server/src/routes/messages.js
+++ server/src/routes/messages.js
@@ -1,4 +1,5 @@
 const router = require("express").Router();
+const { groupBy } = require("../helpers/groupby");
 
 module.exports = db => {
   router.get("/", (request, response) => {
@@ -15,14 +16,43 @@ module.exports = db => {
   router.get("/userMessages", (request, response) => {
     db.query(
       `
-      SELECT *
-      FROM messages
-      WHERE (sender_id = $1 OR receiver_id = $1);
-    `, [request.session["user_id"]])
+        SELECT conversations.*, messages.sender_id, messages.receiver_id, messages.message_text, messages.time_sent
+        FROM conversations
+        JOIN messages ON conversations.id = messages.conversation_id
+        WHERE (messages.sender_id = $1 OR messages.receiver_id = $1);
+        `, [request.session["user_id"]])
       .then(({ rows: messages }) => {
-        return response.json(messages);
+        const result = groupBy(messages, 'id');
+
+        return response.json(result);
       });
   });
 
   return router;
 };
+
+
+
+// router.get("/userMessages", (request, response) => {
+//   db.query(
+//     `
+//     SELECT *
+//     FROM messages
+//     WHERE (sender_id = $1 OR receiver_id = $1)
+//     ORDER BY conversation_id;
+
+//   `, [request.session["user_id"]])
+//     .then(({ rows: messages }) => {
+
+//       const messageIDs = messages.map(message => message.conversation_id);
+
+//       console.log(messages[0].conversation_id);
+
+//       console.log(messageIDs);
+
+//       //get all the conversation ids
+//       //separate the messages by conversation id
+//       //map over and filter 
+//       return response.json(messages);
+//     });
+// });
