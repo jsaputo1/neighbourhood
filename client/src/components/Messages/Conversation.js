f6594ab555acfdb08fdde2f30ca08d05ad997d42
jsaputo1 <59458399+jsaputo1@users.noreply.github.com>
2020-08-06 16:51:39 -0400

    add form to reply to message

--- client/src/components/Messages/Conversation.js
+++ client/src/components/Messages/Conversation.js
@@ -1,12 +1,24 @@
 import React from "react";
 import "../../styles.scss";
+import { Form } from "react-bootstrap";
+import { Button } from "@material-ui/core";
 
 function Conversation(props) {
   return <div className="conversation">
     <figure>
       {props.children}
+      <Form className="message-input">
+        <Form.Group controlId="message">
+          <Form.Control type="firstname" placeholder="Enter message" />
+        </Form.Group>
+        <button>Send</button>
+      </Form>
     </figure>
+
+
   </div>;
 }
 
 export default Conversation;
+
+
