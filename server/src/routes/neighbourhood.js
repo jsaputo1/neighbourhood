6e23575e4e645f35a13e835ac4faaaf5b16395a8
jsaputo1 <59458399+jsaputo1@users.noreply.github.com>
2020-08-07 13:51:05 -0400

    create getUserByID function and add to selectNeighbourhood route

--- server/src/routes/neighbourhood.js
+++ server/src/routes/neighbourhood.js
@@ -1,6 +1,8 @@
 const router = require("express").Router();
 const { findUserCoordinates } = require("../helpers/findUserCoordinates");
 const { findUserByEmail } = require("../helpers/findUserEmail");
+const { findUserByID } = require("../helpers/findUserByID");
+
 
 module.exports = db => {
   router.get("/", (request, response) => {
@@ -15,25 +17,30 @@ module.exports = db => {
   });
 
   router.get("/choices", (request, response) => {
-    const email = 'jsaputo1@gmail.com';
-    findUserCoordinates(email)
+    findUserByID(request.session.user_id)
       .then((user) => {
-        db.query(
-          `
+        findUserCoordinates(user.email)
+          .then((user) => {
+
+            db.query(
+              `
           SELECT * 
           FROM neighbourhoods;
     `
-        ).then(({ rows: data }) => {
-          for (items of data) {
-            items.score = Math.sqrt(Math.pow((user.coordinates.x - items.coordinates.x), 2) + Math.pow((user.coordinates.y - items.coordinates.y), 2));
-          }
-          response.json(data.sort((a, b) => {
-            if (a.score < b.score) return -1;
-            if (a.score > b.score) return 1;
-            return 0;
-          }));
-        });
-      });
+            ).then(({ rows: data }) => {
+              for (items of data) {
+                items.score = Math.sqrt(Math.pow((user.coordinates.x - items.coordinates.x), 2) + Math.pow((user.coordinates.y - items.coordinates.y), 2));
+              }
+              response.json(data.sort((a, b) => {
+                if (a.score < b.score) return -1;
+                if (a.score > b.score) return 1;
+                return 0;
+              }));
+            });
+          });
+
+      }
+      );
   });
 
   router.post("/addNeighbourhood", (request, response) => {
