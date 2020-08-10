ff3e430383d16e64b58c369c96ac67cd5a2ae017
jsaputo1 <59458399+jsaputo1@users.noreply.github.com>
2020-08-10 14:40:10 -0400

    import react strap, add dropdown menu

--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -1,12 +1,16 @@
 import React, { useState } from "react";
 import "../../styles.scss";
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import { Redirect } from "react-router-dom";
+import { Dropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap';
 import axios from "axios";
 import Button from "react-bootstrap/Button";
 
 function Nav(props) {
+
   const [landingRedirect, setlandingRedirect] = useState(false);
+  const [dropdownOpen, setDropdownOpen] = useState(false);
+  const toggle = () => setDropdownOpen(prevState => !prevState);
   const logout = () => {
     axios.post("/users/logout").then((response) => {
       setlandingRedirect(true);
@@ -17,52 +21,66 @@ function Nav(props) {
   if (landingRedirect) {
     return <Redirect to="/login" />;
   }
+
   return (
-    <nav>
-      <h3>LOGO</h3>
-      <ul className="nav-links">
-        <Link className="link-style" to="/home">
-          <li>Home</li>
-        </Link>
-        <Link className="link-style" to="/events">
-          <li>Events</li>
-        </Link>
-        <Link className="link-style" to="/services">
-          <li>Services</li>
-        </Link>
-        <Link className="link-style" to="/Alerts">
-          <li>Alerts</li>
-        </Link>
-        <Link className="link-style" to="/Map">
-          <li>Map</li>
-        </Link>
-        <Link className="link-style" to="/Messages">
-          <li>Messages</li>
-        </Link>
-        <Link className="link-style" to="/Account">
-          <li>Account</li>
-        </Link>
+    <div className="nav-bar">
+      <div className="left-side-nav">
+        <img src="logo" alt="logo" />
+        <div className="dropdown">
+          <Dropdown isOpen={dropdownOpen} toggle={toggle}>
+            <DropdownToggle caret>
+              Dropdown
+        </DropdownToggle>
+            <DropdownMenu>
+              <NavLink to="/home">
+                <DropdownItem>Home</DropdownItem>
+              </NavLink>
+              <NavLink to="/events">
+                <DropdownItem>Events</DropdownItem>
+              </NavLink>
+              <NavLink to="/services">
+                <DropdownItem>Services</DropdownItem>
+              </NavLink>
+              <NavLink to="/alerts">
+                <DropdownItem>Alerts</DropdownItem>
+              </NavLink>
+              <NavLink to="/map">
+                <DropdownItem>Map</DropdownItem>
+              </NavLink>
+            </DropdownMenu>
+          </Dropdown>
+        </div>
+      </div>
+      <div class="right-side-nav">
         {props.user === undefined ? (
           <Link className="link-style" to="/Login">
             <li>Login</li>
           </Link>
         ) : (
-          <div>
-            <li>
-              <h6>
-                {props.user.first_name}
-                {` ${props.user.last_name}`}
-              </h6>
-              <img src={props.user.profile_photo} alt="" />
-            </li>
-            <Button variant="link" onClick={logout}>
-              <li>Logout</li>
-            </Button>
-          </div>
-        )}
-      </ul>
-    </nav>
+            <div>
+              <li>
+                <h6>
+                  {props.user.first_name}
+                  {` ${props.user.last_name}`}
+                </h6>
+                <img src={props.user.profile_photo} alt="" />
+              </li>
+              <Button variant="link" onClick={logout}>
+                <li>Logout</li>
+              </Button>
+              <Link className="link-style" to="/Account">
+                <li>Account</li>
+              </Link>
+              <Link className="link-style" to="/Messages">
+                <li>Messages</li>
+              </Link>
+            </div>
+          )}
+      </div>
+    </div >
   );
-}
+};
 
 export default Nav;
+
+
