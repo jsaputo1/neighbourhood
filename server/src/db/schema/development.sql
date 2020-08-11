e2ccde9c6cf0d214a8d03fd02e0ea4a48e120302
jsaputo1 <59458399+jsaputo1@users.noreply.github.com>
2020-08-11 15:18:12 -0400

    organize events/services/alerts by neighbourhood. add commentented coordinates

--- server/src/db/schema/development.sql
+++ server/src/db/schema/development.sql
@@ -89,23 +89,57 @@ VALUES
 
 INSERT INTO events(user_id, category_id, neighbourhood_id, title, coordinates, time_created, description, event_start, event_time, event_photo)
 VALUES
+
+--Neighbourhood 1 Events
+
+--Neighbourhood 2 Events
+
 (1, 18, 2, 'Party for no reason!!!', '(43.658017, -79.414930)', '2020-08-03 15:40:34-07', 'We are going to party because we are neighbours and we like each other!!! byob', '2020-08-15', '17:00:00', 'https://i.imgur.com/T2WwVfS.png'),
+
+--Neighbourhood 3 Events
+
 (3, 15, 3, 'Crafting Night at Sams!', '(45.54098, -73.625174)', '2020-08-03 14:40:34-07', 'Bring whatever crafting projects you are working on, or just bring random materials and make something up :D I will have snacks for everyone.', '2020-08-15',  '17:00:00', 'https://i.imgur.com/FK8V841.jpg'),
+
 (16, 12, 3, 'Garage Sale, mostly furniture', '(45.537536, -73.619220)', '2020-08-03 16:40:34-07', 'I got a new couch and also some other stuff. Gotta make room. Everything is in good condition.', '2020-08-16', '13:00:00', 'https://i.imgur.com/okB9WKC.jpg'),
+
 (15, 21, 3, 'Cinema en plein air: Back to the future!', '(45.538950, -73.626219)', '2020-08-03 16:40:34-07', 'Nous installons un ecran géant dans notre cour arrière, et allons visionner le film Retour vers le futur, vintage!. Places limitées à 30, écrivez-moi pour réserver la votre!', '2020-08-22', '19:00:00', 'https://i.imgur.com/okB9WKC.jpg');
+
+--Neighbourhood 4 Events
 
 INSERT INTO alerts(user_id, category_id,  neighbourhood_id, title, coordinates, time_created, description, alert_photo)
 VALUES
+
+--Neighbourhood 1 Alerts
+
+--Neighbourhood 2 Alerts
+
 (1, 1, 2, 'I just saw a man trying to break into my car', '(43.652495, -79.412900)', '2020-08-03 15:40:34-07', 'Everyone keep an eye out and check your valuables in your cars tonight. He ran away when I opened my back door. He was tall, skinny, white, and he was wearing a navy sweater, jeans, and a black toque.', 'https://i.imgur.com/Nmx0Qxo.png'),
+
+--Neighbourhood 3 Alerts
+
 (17, 1, 3, 'LOST DOG - Chocolate Lab named Paul', '(45.542615, -73.636909)', '2020-08-01 19:40:34-07', 'My dog, Paul, whom many of you may know, escaped this morning and we still cannot find him. Please message me on here if you see or find him!', 'https://i.imgur.com/iHq8K8Z.jpg'),
+
 (14, 2, 3, 'HUGE NEW POTHOLE', '(45.538166, -73.635435)', '2020-08-01 08:40:34-07', 'I drove into a nice big, new pothole here this morning. BE CAREFUL! IT IS REALLY BIG. I think it damaged my car...', 'https://i.imgur.com/3tVgsra.jpg');
 
+--Neighbourhood 4 Alerts
 
 INSERT INTO services(user_id, category_id, neighbourhood_id, service_offer, title, coordinates, time_created, description, service_photo )
 VALUES
-(1, 4, 2, false, 'Need help weeding my lawn', '(45.539441, -73.621180)', '2020-08-03 15:46:34-07', 'Hey everyone! I need to weed my lawn, but my sciatica is really acting up an I need some help. It should take around 2-3 hours, and I will pay $50', 'https://i.imgur.com/iHq8K8Z.jpg'),
+
+--Neighbourhood 1 Services
+
 (8, 7, 1, true, 'Offering tutoring in HTML, CSS, and Javascript', '(43.656917, -79.422268)', '2020-08-01 18:46:34-07', 'I just completed an amazing Web Development Bootcamp with Lighthouse Labs. If any students are struggling with any coding curriculum, I am here to help! I charge $25/hour... for now.', 'https://i.imgur.com/okB9WKC.jpg'),
+
+--Neighbourhood 2 Services
+
+(1, 4, 2, false, 'Need help weeding my lawn', '(45.539441, -73.621180)', '2020-08-03 15:46:34-07', 'Hey everyone! I need to weed my lawn, but my sciatica is really acting up an I need some help. It should take around 2-3 hours, and I will pay $50', 'https://i.imgur.com/iHq8K8Z.jpg'),
+
+--Neighbourhood 3 Services
+
+
 (15, 5, 3, false, 'Leaky Kitchen Faucet', '(45.539593, -73.636783)', '2020-08-01 15:46:34-07', 'Does anybody know how to fix a leaky faucet? I do not want to call a plumber just for this...', 'https://i.imgur.com/LpaY82x.png');
+
+--Neighbourhood 4 Services
 
 
 INSERT INTO subscriptions(user_id, category_id)
@@ -131,13 +165,11 @@ VALUES
 (1, 13),
 (4, 13);
 
-
 INSERT INTO conversations(user_one, user_two)
 VALUES
 (1, 2),
 (1, 3),
 (2, 3);
-
 
 INSERT INTO messages(conversation_id, sender_id, receiver_id, message_text, time_sent)
 VALUES
@@ -148,59 +180,110 @@ VALUES
 (3, 2, 3, 'Thank you for the warning about the pothole. I avoided it!', '2020-08-06T04:52:25.931Z'),
 (3, 3, 2, 'My pleasure!', '2020-08-06T06:52:25.931Z7');
 
--- -- Little Italy Coordinates
+-- Little Italy Coordinates
+-- Little Italy  Users
+-- (43.657603, -79.411882) --In use by user 2 John
 -- (43.65878, -79.42172)
--- (43.65903, -79.41303)
--- (43.65125, -79.41975)
--- (43.65195, -79.41786)
--- (43.65111, -79.41636)
 -- (43.65277, -79.41584)
+-- (43.65768, -79.41084)
+-- (43.65125, -79.41975)
+-- (43.65903, -79.41303)
 -- (43.65522, -79.41749)
+
+-- Little Italy Alerts
 -- (43.65732, -79.41678)
 -- (43.65277, -79.41584)
--- (43.65257, -79.41342)
 -- (43.65682, -79.41461)
--- (43.65903, -79.41303)
+
+-- Little Italy Events
 -- (43.65868, -79.40998)
--- (43.65768, -79.41084)
 -- (43.65411, -79.40807)
+-- (43.655573, -79.415224) -- Central point of neighbourhood ID
+-- (43.65111, -79.41636)
 
--- --Queen West Coordinates
--- (43.64666, -79.41574)
--- (43.64535, -79.41519)
+-- Little Italy Services
+-- (43.65195, -79.41786)
+-- (43.65257, -79.41342)
+-- (43.656917, -79.422268) -- In use
+
+--Queen West Coordinates
+-- Queen West Users
 -- (43.64504, -79.41477)
+-- (43.64666, -79.41574)
 -- (43.64419, -79.41273)
--- (43.64703, -79.40885)
--- (43.64617, -79.40885)
--- (43.64571, -79.40735)
--- (43.64533, -79.40602)
 -- (43.64794, -79.40546)
+-- (43.64617, -79.40885)
 
--- --Jarry Coordinates
--- (45.54331, -73.63609)
--- (45.54171, -73.63641)
--- (45.53796, -73.63766)
--- (45.53588, -73.64042)
--- (45.53531, -73.63349)
--- (45.54081, -73.63317)
--- (45.54301, -73.63066)
+-- Queen West Alerts
+-- (43.652495, -79.412900) -- In use
+-- (43.64533, -79.40602)
+-- (43.646315, -79.408272) -- Central point of neighbourhood 
+
+-- Queen West Events
+-- (43.658017, -79.414930) -- In use
+-- (43.64535, -79.41519)
+-- (43.64571, -79.40735)
+
+-- Queen West Services
+-- (43.64703, -79.40885)
+-- (45.539441, -73.621180) -- In use
+
+-- Jarry Coordinates
+-- Jarry Users
+-- (45.542915, -73.634951) -- In use by user 1 Graham
+-- (45.539652, -73.629813) -- In use by user 3 Sam
+-- (45.539390, -73.631162) -- Central point of neighbourhood ID
 -- (45.54239, -73.62577)
 -- (45.53979, -73.62693)
 -- (45.53856, -73.62259)
--- (45.53714, -73.62186)
+-- (45.53588, -73.64042)
 -- (45.53409, -73.62345)
--- (45.53316, -73.62229)
+-- (45.54301, -73.63066)
+
+-- Jarry Alerts
+-- (45.542615, -73.636909) -- In use
+-- (45.538166, -73.635435) -- In use
 -- (45.53629, -73.61912)
+-- (45.54081, -73.63317)
+-- (45.53714, -73.62186)
+
+-- Jarry Events
+-- (45.54098, -73.625174) -- In use
+-- (45.537536, -73.619220) -- In use
+-- (45.538950, -73.626219) -- In Use
+-- (45.54171, -73.63641)
+-- (45.53531, -73.63349)
+
+-- Jarry Services
+-- (45.539593, -73.636783) -- In use
+-- (45.54331, -73.63609)
+-- (45.53796, -73.63766)
+-- (45.53316, -73.62229)
 -- (45.53877, -73.61618)
 
--- --Villeray Coordinates
--- (45.55211, -73.62014)
--- (45.54745, -73.62929)
--- (45.54436, -73.62826)
--- (45.54604, -73.62006)
--- (45.54986, -73.61891)
--- (45.54821, -73.61487)
--- (45.54309, -73.61753)
+-- Villeray Coordinates
+-- Villeray Users
+-- (45.547282, -73.620562) -- Central point of neighbourhood ID
 -- (45.54367, -73.61444)
--- (45.54526, -73.61208)
+-- (45.54436, -73.62826)
+-- (45.54821, -73.61487)
+-- (45.54986, -73.61891)
+-- (45.5496, -73.6161)
+-- (45.54345, -73.61116)
 
+-- Villeray Alerts
+-- (45.55211, -73.62014)
+-- (45.54526, -73.61208)
+-- (45.54623, -73.60876)
+-- (45.54564, -73.62411)
+
+-- Villeray Events
+-- (45.54604, -73.62006)
+-- (45.54745, -73.62929)
+-- (45.54731, -73.62317)
+-- (45.54725, -73.61834)
+
+-- Villeray Services
+-- (45.54309, -73.61753)
+-- (45.54617, -73.61596)
+-- (45.54413, -73.62113)
