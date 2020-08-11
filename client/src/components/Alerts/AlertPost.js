419a2c96e13cf9ef291b0576699dc730828fd3ed
Graham Mothersill <graham.mothersill@gmail.com>
2020-08-11 15:48:18 -0700

    restyle Alert Posts and Service Posts

--- client/src/components/Alerts/AlertPost.js
+++ client/src/components/Alerts/AlertPost.js
@@ -33,7 +33,7 @@ function AlertPost(props) {
     };
     return (
         <div className="box">
-            <div className="card gedf-card">
+            <div className="card">
                 <div className="card-header">
                     <div className="d-flex justify-content-between align-items-center">
                         <div className="mr-2">
@@ -44,6 +44,7 @@ function AlertPost(props) {
                                 alt=""
                             ></img>
                         </div>
+
                         <div className="ml-2">
                             <div className="h5 m-0">
                                 {props.user_first_name} {props.user_last_name}
@@ -52,63 +53,67 @@ function AlertPost(props) {
                                 {" " + moment(props.time_created).fromNow()}
                             </div>
                         </div>
-                    </div>
+                        <div id="SA-post-message-or-new">
 
-                    {props.current_user_id === props.user_id ?
+                            {props.current_user_id === props.user_id ?
 
-                        <div>
-                            <Button
-                                variant="contained"
-                                color="secondary"
-                                onClick={props.handleOpenDelete}
-                                className={classes.button}
-                                startIcon={<DeleteIcon />}>
-                                DELETE Alert
+                                <div>
+                                    <Button
+                                        variant="contained"
+                                        color="secondary"
+                                        onClick={props.handleOpenDelete}
+                                        className={classes.button}
+                                        startIcon={<DeleteIcon />}>
+                                        DELETE Alert
 </Button>
-                            <Modal
-                                aria-labelledby="Moo"
-                                aria-describedby="Moo"
-                                className={props.modalClass}
-                                open={props.openDelete}
-                                onClose={props.handleCloseDelete}
-                                closeAfterTransition
-                                BackdropComponent={Backdrop}
-                                BackdropProps={{
-                                    timeout: 500,
-                                }}
-                            >
-                                <Fade in={props.openDelete}>
-                                    <div className={props.paperClass}>
-                                        <h2 id="transition-modal-title">Are you sure you would like to delete this Alert?</h2>
-                                        <Form data-message={props.id} onSubmit={props.deleteSubmitHandler}>
+                                    <Modal
+                                        aria-labelledby="Moo"
+                                        aria-describedby="Moo"
+                                        className={props.modalClass}
+                                        open={props.openDelete}
+                                        onClose={props.handleCloseDelete}
+                                        closeAfterTransition
+                                        BackdropComponent={Backdrop}
+                                        BackdropProps={{
+                                            timeout: 500,
+                                        }}
+                                    >
+                                        <Fade in={props.openDelete}>
+                                            <div className={props.paperClass}>
+                                                <h2 id="transition-modal-title">Are you sure you would like to delete this Alert?</h2>
+                                                <Form data-message={props.id} onSubmit={props.deleteSubmitHandler}>
 
-                                            <Button variant="contained" color="secondary" type="submit">
-                                                Confirm
+                                                    <Button variant="contained" color="secondary" type="submit">
+                                                        Confirm
 </Button>
-                                            <Button onClick={props.handleCloseDelete} variant="contained" color="primary" type="button">
-                                                Cancel
+                                                    <Button onClick={props.handleCloseDelete} variant="contained" color="primary" type="button">
+                                                        Cancel
 </Button>
-                                        </Form>
-                                    </div>
-                                </Fade>
-                            </Modal>
+                                                </Form>
+                                            </div>
+                                        </Fade>
+                                    </Modal>
+                                </div>
+                                :
+                                <Link id="SA-post-message" className="message-icon" to={{ pathname: "/newmessage" }}>
+                                    <i
+                                        class="fa fa-comment-o fa-2x"
+                                        aria-hidden="true"
+                                        onClick={() => setReceiver(receiverObject)}
+                                    ></i>
+                                </Link>
+                            }
                         </div>
-                        :
-                        <Link className="message-icon" to={{ pathname: "/newmessage" }}>
-                            <i
-                                class="fa fa-comment-o fa-2x"
-                                aria-hidden="true"
-                                onClick={() => setReceiver(receiverObject)}
-                            ></i>
-                        </Link>
-                    }
+
+
+                    </div>
 
                 </div>
             </div>
             <div className="card-body">
                 <ul className="list-group list-group-flush">
-                    <li className="list-group-item">
-                        <h5 className="card-title">{props.post_title}</h5>
+                    <li id="SA-post-title" className="list-group-item">
+                        <h3 className="card-title">{props.post_title}</h3>
                     </li>
                     <li className="list-group-item">
                         <img className="post-photo" src={props.post_photo} alt=""></img>
