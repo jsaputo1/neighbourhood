31cebd97cbf55fd13bfd2c47ff7a638951ca13f6
jsaputo1 <59458399+jsaputo1@users.noreply.github.com>
2020-08-12 22:12:39 -0400

    add banner image to landing

--- client/src/components/Landing/Landing.js
+++ client/src/components/Landing/Landing.js
@@ -1,44 +1,27 @@
 import React from "react";
-
-// @material-ui/core components
-import { makeStyles } from "@material-ui/core/styles";
-
-//Material-kit-component styling and components (try to avoid understanding it, too confusing...)
-import styles from "./Material-kit-components/landingPage.js";
-import Parallax from "./Material-kit-components/Parallax.js";
-
-// our own style sheet
+import { Button } from "@material-ui/core";
 import "../../styles.scss";
 
-const useStyles = makeStyles(styles);
-
 function Landing(props) {
-  const classes = useStyles();
-
   return (
     <div>
-      <Parallax filter image={require("../../assets/img/neighbours.jpg")}>
-        <div className={classes.container}>
-          <div className="main-container">
-            {/* <h1>Join your private neighbourhood social network!</h1>
-          <h4>
-            How many neighbours do you know? The growth of cities, mobility, and
-            distrust have weakened our relationships with our neighbours. You
-            have now the opportunity to connect and create a community.
-          </h4> */}
-            <img src="/title.png" alt="CupOSugah"></img>
-            <br />
-            <div className="button-container">
-              <a class="btn btn-primary " href="/login" role="button">
-                LOGIN
-              </a>
-              <a class="btn btn-primary" href="/register" role="button">
-                REGISTER
-              </a>
-            </div>
-          </div>
+      <div className="landing-container">
+        <div className="landing-image">
+          <img src="/images/banner-dark-blue.png" alt="CupOSugah"></img>
         </div>
-      </Parallax>
+        <h2 className="landing-title">Your neighbourhood social network!</h2>
+        <div className="mobile-landing-image">
+          <img src="/logo.png"></img>
+        </div>
+        <div className="button-container">
+          <Button variant="contained" color="primary" href="/login">
+            LOGIN
+          </Button>
+          <Button variant="contained" color="primary" href="/register">
+            REGISTER
+          </Button>
+        </div>
+      </div>
     </div>
   );
 }
